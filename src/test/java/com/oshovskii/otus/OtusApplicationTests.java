d3581e9379f14a8d375814e659e540973522e560
Qwerty <rosna-m@mail.ru>
2021-11-23 19:01:52 +0800

    Commit 1. add project structure

---
+++ src/test/java/com/oshovskii/otus/OtusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshovskii.otus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
